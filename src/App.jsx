721fe25a4ed79fa5f2956356ff42491679254401
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-06-18 16:19:38 +0530

    Implement routing and layout structure in App component; integrate BrowserRouter in main entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/NavBar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MoviesDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import Mybookings from "./pages/MyBookings";
+import Favourite from "./pages/Favourite";
+import Footer from "./components/Footer";
+
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MoviesDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<Mybookings />} />
+        <Route path="/favourite" element={<Favourite />} />
+      </Routes>
+
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
