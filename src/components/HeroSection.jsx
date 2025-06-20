9f2ca89dbfcf5bd6dec9c1dcabd4877bfb2972dc
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-06-20 21:07:17 +0530

    Add HeroSection component: create a new HeroSection component for the homepage, featuring movie details and a navigation button to explore movies.

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { ArrowRight, CalendarIcon, ClockIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+const HeroSection = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("./backgroundImage.png")] bg-cover bg-center h-screen'>
+      <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20" />
+
+      <h1 className="text-5xl md:text-[80px] md:leading-18 font-semibold max-w-110">
+        Gurdians Of The Galaxy
+      </h1>
+
+      <div className="flex items-center gap-4 text-green-300">
+        <span>Action | Adventure | Sci-Fi</span>
+        <div className="flex items-center gap-1">
+          <CalendarIcon className="w-4.5 h-4.5" /> 2018
+        </div>
+
+        <div className="flex items-center gap-1">
+          <ClockIcon className="w-4 h-4.5" /> 2h 8m
+        </div>
+      </div>
+
+      <p className="max-w-md text-green-300">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, fugiat.
+        Excepturi neque alias nam quidem autem. Veniam deserunt quo aspernatur
+        voluptatem, officia et earum sequi sint? In eveniet optio laboriosam.
+      </p>
+      <button
+        onClick={() => {
+          navigate("/movies");
+        }}
+        className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+      >
+        Explore Movies
+        <ArrowRight className="w-5 h-5" />
+      </button>
+    </div>
+  );
+};
+
+export default HeroSection;
