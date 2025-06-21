28344eae10e27a479a8f6e8c6c2ded68b3c9ebb7
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-06-21 08:14:44 +0530

    Enhance FeaturedSection component: add BlurCircle for visual effect and implement a "Show More" button to navigate to the movies page, improving user interaction.

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -1,12 +1,14 @@
 import { ArrowRight } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import BlurCircle from "./BlurCircle";
 
 const FeaturedSection = () => {
   const navigate = useNavigate();
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
       <div className="relative flex items-center justify-between pt-20 pb-10">
+        <BlurCircle top="0" right="0" />
         <p className="text-gray-300 font-medium text-lg">Now Showing</p>
         <button
           onClick={() => navigate("/movies")}
@@ -17,7 +19,17 @@ const FeaturedSection = () => {
         </button>
       </div>
       <div></div>
-      <div></div>
+      <div className="flex justify-center mt-20">
+        <button
+          onClick={() => {
+            navigate("/movies");
+            scrollTo(0, 0);
+          }}
+          className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer"
+        >
+          Show More
+        </button>
+      </div>
     </div>
   );
 };
